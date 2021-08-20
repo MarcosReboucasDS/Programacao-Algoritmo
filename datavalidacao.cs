deecad9e50eed4c7b5e65210d1dae8bc759a1dbb
Marcos Rebouças Duarte da Silva <80132434+MarcosReboucasDS@users.noreply.github.com>
2021-08-20 19:38:23 -0300

    Add files via upload

---
+++ datavalidacao.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class MainClass
+{
+    public struct Data
+    {
+        public int dia, mes, ano;
+    }
+    public static void Main(string[] args)
+    {
+        Data dataNasc;
+
+        Console.WriteLine("Data do seu Nascimento");
+        Console.Write("Dia: ");
+        dataNasc.dia = int.Parse(Console.ReadLine());
+        Console.Write("Mês: ");
+        dataNasc.mes = int.Parse(Console.ReadLine());
+        Console.Write("Ano: ");
+        dataNasc.ano = int.Parse(Console.ReadLine());
+
+        if (dataNasc.dia >= 32 || dataNasc.dia < 1 || dataNasc.mes >= 13 || dataNasc.mes >= 0)
+        {
+            Console.WriteLine("Data Inválida! ");
+        }
+        else if (dataNasc.mes == 02 && dataNasc.dia > 29 && dataNasc.ano % 4 == 0 && dataNasc.ano % 100 != 0 || dataNasc.ano % 400 == 0)
+        {
+            Console.WriteLine("Data Inválida! ");
+        }
+        else if (dataNasc.mes == 04 || dataNasc.mes == 06 || dataNasc.mes == 09 || dataNasc.mes == 11 && dataNasc.dia >= 31)
+        {
+            Console.WriteLine("Data Inválida! ");
+        }
+        else if (dataNasc.mes == 02 && dataNasc.dia > 28)
+        {
+            Console.WriteLine("Data Inválida! ");
+
+        }
+        else
+        {
+
+            Console.Write("\n\nData de Nascimento informada: ");
+            Console.WriteLine("{0}/{1}/{2} \n\n", dataNasc.dia, dataNasc.mes, dataNasc.ano);
+        }
+        Console.ReadKey();
+    }
+}
---
+++ notas15.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace notas15
+{
+    class Program
+    {
+        public struct Aluno
+        {
+            public string nome;
+            public int nota1;
+            public int nota2;
+            public double media;
+        }
+        static void Main(string[] args)
+        {
+            const int TAM = 15;
+            Aluno[] aluno = new Aluno[TAM];
+            for(int x = 0; x < TAM; x++)
+            {
+                Console.WriteLine("Nome do {0}° aluno: ", x);
+                aluno[x].nome = Console.ReadLine();
+                Console.WriteLine("Primeira nota: ");
+                aluno[x].nota1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Segunda nota: ");
+                aluno[x].nota1 = int.Parse(Console.ReadLine());
+            }
+        }
+    }
+}
